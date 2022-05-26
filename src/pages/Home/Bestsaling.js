a8e2f6f76810c2dbb6b1e99f7c744c00295da44f
Amortha <amitsharma1998k@gmail.com>
2022-05-26 20:11:03 +0600

    commit

--- src/pages/Home/Bestsaling.js
+++ src/pages/Home/Bestsaling.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 const Bestsaling = () => {
+        const [bestsaling, setBastsaling] = useState([]);
+        useEffect(()=>{
+            fetch('Bestsaling.json')
+            .then(res => res.json())
+            .then(data => setBastsaling(data))
+        }, [])
     return (
         <div>
             
