40f025c57409c50daed676abe052096f05abe362
Amortha <amitsharma1998k@gmail.com>
2022-05-26 21:09:27 +0600

    commit

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
 
- apiKey: "AIzaSyAKKscBKm81lwJ2A_7maTakH7Nz0fBdVEM",
-  authDomain: "keeway-7f9fb.firebaseapp.com",
-  projectId: "keeway-7f9fb",
-  storageBucket: "keeway-7f9fb.appspot.com",
-  messagingSenderId: "406712791641",
-  appId: "1:406712791641:web:fef7f80a8c9bc062b23748",
+  apiKey: ,
+  authDomain: ,
+  projectId: ,
+  storageBucket: ,
+  messagingSenderId: ,
+  appId: ,
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
