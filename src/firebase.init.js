8dd4d25682f0e71ed1eb7424b93445ede5e345a4
Amortha <amitsharma1998k@gmail.com>
2022-05-26 21:04:25 +0600

    commit

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -3,18 +3,15 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
 
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
-  
+ apiKey: "AIzaSyAKKscBKm81lwJ2A_7maTakH7Nz0fBdVEM",
+  authDomain: "keeway-7f9fb.firebaseapp.com",
+  projectId: "keeway-7f9fb",
+  storageBucket: "keeway-7f9fb.appspot.com",
+  messagingSenderId: "406712791641",
+  appId: "1:406712791641:web:fef7f80a8c9bc062b23748",
 };
 // Initialize Firebase
-
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-
 
 export default auth;
