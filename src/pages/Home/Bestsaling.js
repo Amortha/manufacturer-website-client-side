1236b8b880e77995a5b81ebd67ab88d653e5f9e7
Amortha <amitsharma1998k@gmail.com>
2022-05-26 20:26:01 +0600

    commit

--- src/pages/Home/Bestsaling.js
+++ src/pages/Home/Bestsaling.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Bestsaling = () => {
         const [bestsaling, setBastsaling] = useState([]);
@@ -11,12 +11,9 @@ const Bestsaling = () => {
         <div>
         <h2 className="text-3xl text-center font-bold my-5 text-primary">Our Parts</h2>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-10'>
-            {
-               bestsaling.map((p, index) =><Bestsaling
-                    key={index}
-                    part={p}
-                />) 
-            }
+        {
+                    bestsaling.map(bastsaling =><Bestsaling></Bestsaling>)
+                }
         </div>
     </div>
     );
